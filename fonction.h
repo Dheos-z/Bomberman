103adbb654db738d9f6db59272a7a1d9ee4b0761
Zak <yakdi.zak@gmail.com>
2017-04-08 09:36:16 +0000

    Suppression de fonction.*

--- fonction.h
+++
@@ -1,8 +0,0 @@
-#ifndef FONCTION_H
-#define FONCTION_H
-
-void remplissage(int map[NB_CASES][NB_CASES]);
-void affichage(int map[NB_CASES][NB_CASES]);
-void initPerso(int map[NB_CASES][NB_CASES]);
-
-#endif
